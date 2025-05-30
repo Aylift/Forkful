699e16288de2ed7c07e55e43e9cd66d0709d11b1
Aylift <adam.martka@gmail.com>
2025-05-30 17:53:31 +0200

    created custom user model, changed settings, added serializers, added basic apiview for users, updated urls

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+# Możemy później rozwinąć CustomUser, albo zrobić kolejny model np. Profile z One-to-One do CustomUser i tam wrzucić dodatkowe fieldy
+class CustomUser(AbstractUser):
+    phone_number = models.CharField(max_length=20, blank=True)
+    address = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.username
