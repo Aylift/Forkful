700059f4d04780b4fe316fdc0d17be4c8c91f04d
Aylift <adam.martka@gmail.com>
2025-06-06 18:13:29 +0200

    add nutrients serializers, views and urls

--- backend/nutrients/urls.py
+++ backend/nutrients/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import IngredientListView, IngredientDetailView
+from .views import IngredientListView, IngredientDetailView, NutrientListView, NutrientDetailView
 
 
 urlpatterns = [
     path('ingredients/', IngredientListView.as_view(), name='ingredient_list'),
-    path('ingredients/<int:pk>/', IngredientDetailView.as_view(), name='ingredient_detail')
+    path('ingredients/<int:pk>/', IngredientDetailView.as_view(), name='ingredient_detail'),
+    path('nutrients/', NutrientListView.as_view(), name='nutrient_list'),
+    path('nutrients/<int:pk>/', NutrientDetailView.as_view(), name='nutrient_detail')
 ]
--- backend/nutrients/views.py
+++ backend/nutrients/views.py
@@ -1,8 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from drf_spectacular.utils import extend_schema
-from .models import Ingredient
-from .serializers import IngredientSerializer
+from .models import Ingredient, Nutrient
+from .serializers import IngredientSerializer, NutrientSerializer
 from rest_framework.generics import get_object_or_404
 from rest_framework import status
 
@@ -52,3 +52,51 @@ class IngredientDetailView(APIView):
         ingredient = get_object_or_404(Ingredient, pk=pk)
         ingredient.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class NutrientListView(APIView):
+    @extend_schema(
+        summary="List of all nutrients",
+        responses={200: NutrientSerializer(many=True)}
+    )
+    def get(self, request):
+        nutrients = Nutrient.objects.all()
+        serializer = NutrientSerializer(nutrients, many=True)
+        return Response(serializer.data)
+
+    @extend_schema(
+        summary="Add nutrient",
+        request=NutrientSerializer,
+        responses={201: NutrientSerializer}
+    )
+    def post(self, request):
+        serializer = NutrientSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class NutrientDetailView(APIView):
+    @extend_schema(
+        summary="Edit nutrient",
+        request=NutrientSerializer,
+        responses={200: NutrientSerializer}
+    )
+    def put(self, request, pk):
+        nutrient = get_object_or_404(Nutrient, pk=pk)
+        serializer = NutrientSerializer(Nutrient, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @extend_schema(
+        summary="Delete nutrient",
+        responses={204: None}
+    )
+    def delete(self, request, pk):
+        nutrient = get_object_or_404(Nutrient, pk=pk)
+        nutrient.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
