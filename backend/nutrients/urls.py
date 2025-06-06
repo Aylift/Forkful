20bf7954235cfa2644c8f4a879d1973a7819b7a4
Aylift <adam.martka@gmail.com>
2025-06-06 18:51:17 +0200

    add ingredient_nutrients serializers, views and urls

--- backend/nutrients/urls.py
+++ backend/nutrients/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import IngredientListView, IngredientDetailView, NutrientListView, NutrientDetailView
+from .views import IngredientListView, IngredientDetailView, NutrientListView, NutrientDetailView, IngredientNutrientListView, IngredientNutrientDetailView
 
 
 urlpatterns = [
     path('ingredients/', IngredientListView.as_view(), name='ingredient_list'),
     path('ingredients/<int:pk>/', IngredientDetailView.as_view(), name='ingredient_detail'),
     path('nutrients/', NutrientListView.as_view(), name='nutrient_list'),
-    path('nutrients/<int:pk>/', NutrientDetailView.as_view(), name='nutrient_detail')
+    path('nutrients/<int:pk>/', NutrientDetailView.as_view(), name='nutrient_detail'),
+    path('ingredient_nutrients/', IngredientNutrientListView.as_view(), name='ingredient_nutrient_list'),
+    path('ingredient_nutrients/<int:pk>/', IngredientNutrientDetailView.as_view(), name='ingredient_nutrient_detail')
 ]
